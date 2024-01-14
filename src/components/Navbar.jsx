fb26d917948c5a0b623016af4421dac2b4673234
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-01-14 22:04:50 +0530

    added conditional rendering based on if a user is logged in or not, that will determine what is rendered in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 import './Navbar.css';
 import Temple from '../assets/temple.svg'
@@ -7,6 +8,7 @@ import Temple from '../assets/temple.svg'
 export default function Navbar() {
 
     const {logout, isPending} = useLogout();
+    const {user} = useAuthContext();
 
     return (
         <div className='navbar'>
@@ -15,12 +17,18 @@ export default function Navbar() {
                     <img src={Temple} alt="Logo" />
                     <span>Project Workshop</span>
                 </li>
-                <li><Link to='/login'>Login</Link></li>
-                <li><Link to='/signup'>Sign Up</Link></li>
-                <li>
-                    {!isPending && <button className='btn' onClick={logout}>Logout</button>}
-                    {isPending && <button className='btn' disabled>Logging out</button>}
-                </li>
+                {!user && (
+                    <>
+                        <li><Link to='/login'>Login</Link></li>
+                        <li><Link to='/signup'>Sign Up</Link></li>
+                    </>
+                )}
+                {user && (
+                    <li>
+                        {!isPending && <button className='btn' onClick={logout}>Logout</button>}
+                        {isPending && <button className='btn' disabled>Logging out</button>}
+                    </li>
+                )}
             </ul>
         </div>
     )
