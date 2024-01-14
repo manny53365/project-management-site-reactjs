cc05feb6c7ca64d5630652c6f3f605421dc0e9ca
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-01-14 17:54:57 +0530

    added logic to handle file uploads

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -7,9 +7,38 @@ export default function Signup() {
     const [password, setPassword] = useState('');
     const [displayName, setDisplayName] = useState('');
     const [thumbnail, setThumbnail] = useState(null);
+    const [thumbnailError, setThumbnailError] = useState(null);
+
+    const handleFileChange = event => {
+        setThumbnail(null);
+        let selectedImg = event.target.files[0];
+        console.log(selectedImg);
+
+        if (!selectedImg){
+            setThumbnailError('Please select a file');
+            return;
+        };
+        if (!selectedImg.type.includes('image')){
+            setThumbnailError('Selected file must be an image');
+            return;
+        };
+        if (selectedImg.size > 100000){
+            setThumbnailError('Image file size must be less than 100kb');
+            return;
+        };
+
+        setThumbnailError(null);
+        setThumbnail(selectedImg);
+        console.log('Thumbnail updated')
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`${email} \n ${password} \n ${displayName} \n ${JSON.stringify(thumbnail)}`);
+    }
     
     return (
-        <form className='auth-form'>
+        <form className='auth-form' onSubmit={handleSubmit}>
             <h2>Sign Up</h2>
             <label>
                 <span>Email:</span>
@@ -43,9 +72,10 @@ export default function Signup() {
                 <input
                 required
                 type="file"
-                // onChange={event => setThumbnail(event.target.value)}
+                onChange={handleFileChange}
                 // value={thumbnail}
                 />
+                {thumbnailError && <div className='error'>{thumbnailError}</div>}
             </label>
             <button className='btn'>Sign up</button>
         </form>
