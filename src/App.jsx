96171ce8d9d4ee438bf8f32513c9181b7af97d56
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-01-14 15:19:45 +0530

    added pages to each route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import {BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import './App.css'
+import Dashboard from './pages/dashboard/Dashboard';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Create from './pages/create/Create';
+import Project from './pages/project/Project';
 
 function App() {
   return (
     <div className="App">
-        
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/'>
+              <Dashboard />
+            </Route>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route path='/signup'>
+              <Signup />
+            </Route>
+            <Route path='/create'>
+              <Create/>
+            </Route>
+            <Route path='/project'>
+              <Project/>
+            </Route>
+          </Switch>
+        </BrowserRouter>
     </div>
   );
 }
