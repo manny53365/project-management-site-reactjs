57222e84589b8389c06df5d6066331671a4fc5f7
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-01-14 23:58:15 +0530

    created project obj that will contain all of the details and that object will be stored in the projects collection of the db

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import Select from 'react-select';
 import { useCollection } from '../../hooks/useCollection';
 import './Create.css';
+import { timestamp } from '../../firebase/config';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 const categories = [
     {value: 'development', label: 'Development'},
@@ -13,6 +15,7 @@ const categories = [
 export default function Create() {
     const { documents } = useCollection('users');
     const [users, setUsers] = useState([]);
+    const { user } = useAuthContext();
 
     const [name, setName] = useState('');
     const [details, setDetails] = useState('');
@@ -43,7 +46,27 @@ export default function Create() {
             return;
         };
 
-        console.log(name, details, dueDate, category, assignedUsers);
+        const createdBy = {
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+            id: user.uid
+        }
+
+        const assignedUsersList = assignedUsers.map(user => {
+            return {displayName: user.value.displayName, photoURL: user.value.photoURL, id: user.value.id};
+        });
+
+        const project = {
+            name,
+            details,
+            category: category.value,
+            dueDate: timestamp.fromDate(new Date(dueDate)),
+            comments: [],
+            createdBy,
+            assignedUsersList
+        }
+
+        console.log(project);
     }
 
     return (
