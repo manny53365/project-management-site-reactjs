bc5937629fb6961f1914f376ae298ecac9d8421d
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-01-15 13:27:57 +0530

    added styling to the dashboard to appear as cards and populated some project details to the card

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,11 +1,25 @@
+import { Link } from 'react-router-dom';
+import Avatar from './Avatar';
 import './ProjectList.css';
 
 export default function ProjectList({ docs }) {
     return (
-        <div>
+        <div className='project-list'>
             {docs.length === 0 && <p className='error'>No projects yet!</p>}
             {docs.map(doc => (
-                <p key={doc.id}>{doc.name}</p>
+                <Link to={`/projects/${doc.id}`} key={doc.id}>
+                    <h4>{doc.name}</h4>
+                    <p>{`Due by ${doc.dueDate.toDate().toDateString()}`}</p>
+                    <div className='assigned-to'>
+                        <ul>
+                            {doc.assignedUsersList.map(user => (
+                                <li key={user.photoURL}>
+                                    <Avatar src = {user.photoURL}/>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </Link>
             ))}
         </div>
     )
