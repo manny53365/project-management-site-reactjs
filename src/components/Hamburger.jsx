1af358f9cd5e2556966b283e91aa876802f43d90
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-01-31 18:00:44 +0530

    added starter code for hamburger menu

---
+++ src/components/Hamburger.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hamburger() {
+  return (
+    <div className='hamburger'>
+        <div className="burger"></div>
+        <div className="burger"></div>
+        <div className="burger"></div>
+    </div>
+  )
+};
