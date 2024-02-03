514aae53fbe016717721eba0584dab8a1f9a8cea
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-02-03 14:13:26 +0530

    removed unnecessary code

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -4,27 +4,18 @@ export default function Hamburger({ onClick, isOpen }) {
   return (
     <div className='hamburger' onClick={onClick}>
         <div className="burger burger1" style={{
-            transform: isOpen ? 'rotate(45deg)' : 'rotate(0)',
-            position: 'fixed',
-            top: 40,
-            left: 15
+            transform: isOpen ? 'rotate(45deg)' : 'rotate(0)'
         }}
         ></div>
         <div className="burger burger2" 
         style={{
             transform: isOpen ? 'translateX(100%)' : 'translateX(0)',
-            opacity: isOpen ? 0 : 1,
-            position: 'fixed',
-            top: 50,
-            left: 15
+            opacity: isOpen ? 0 : 1
         }}
         ></div>
         <div className="burger burger3" style={{
             transform: isOpen ? 'rotate(-45deg)': 'rotate(0)',
-            marginTop: isOpen ? '-15px' : '5px',
-            position: 'fixed',
-            top: 57,
-            left: 15
+            marginTop: isOpen ? '-20px' : '0'
         }}
         ></div>
     </div>
